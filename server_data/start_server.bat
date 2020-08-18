33e3c1a22ac23be1e7749382b9e7ae8ccae3c22d
Dscha <Eggi2105@googlemail.com>
2020-08-18 17:47:57 +0200

    start_server.bat updated (no changes to the path needed)

--- server_data/start_server.bat
+++ server_data/start_server.bat
@@ -3,20 +3,16 @@
 taskkill /IM arma3server_x64.exe /F
 taskkill /IM bec.exe /F
 
-:: ADJUST ME
-SET ArmaFolder=D:\#A3-Server
-D:
-:: DO NOT TOUCH ME
-CD %ArmaFolder%
-SET ExtFolder=%ArmaFolder%\#ServerData
-SET ArmaUserFolder=%ExtFolder%\AN-User
-SET ArmaConfigFolder=%ExtFolder%\AN-Config
+
+SET CurrentFolder=%~dp0
+SET DataFolder=%CurrentFolder%#ServerData
+SET ArmaUserFolder=%DataFolder%\AN-User
+SET ArmaConfigFolder=%DataFolder%\AN-Config
 
 
 
 @echo on
-start "Server_AN" /high "arma3server_x64.exe" -filepatching -enableHT -malloc=system -autoInit -port=2302 -loadMissionToMemory -noPause -maxmem=3071 -noSound -bandwidthAlg=2 "-servermod=@anarchy_server;" "-mod=@anarchy_client;@CBA_A3;@task_force_radio;" -config=%ArmaConfigFolder%\server_config_AN.cfg -cfg=%ArmaConfigFolder%\basic_AN.cfg -profiles=%ArmaUserFolder% -name=Server_AN
-::   -ServerMod=%ExtFolder%\@IFAD_Server;
+start "Server_AN" /high "arma3server_x64.exe" -filepatching -enableHT -malloc=system -autoInit -port=2302 -loadMissionToMemory -noPause -maxmem=3071 -noSound -bandwidthAlg=2 "-servermod=@anarchy_server;" "-mod=@anarchy_client" -config=%ArmaConfigFolder%\server_config_AN.cfg -cfg=%ArmaConfigFolder%\basic_AN.cfg -profiles=%ArmaUserFolder% -name=Server_AN
 ::   /affinity F 
 
 :: timeout 5
