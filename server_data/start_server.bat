fed8e00120faf0be63048d6b16dfc0447f68e9e2
Dscha <Eggi2105@googlemail.com>
2020-08-18 18:27:29 +0200

    (hopefully) final update to start_server.bat

--- server_data/start_server.bat
+++ server_data/start_server.bat
@@ -12,7 +12,7 @@ SET ArmaConfigFolder=%DataFolder%\AN-Config
 
 
 @echo on
-start "Server_AN" /high "arma3server_x64.exe" -filepatching -enableHT -malloc=system -autoInit -port=2302 -loadMissionToMemory -noPause -maxmem=3071 -noSound -bandwidthAlg=2 "-servermod=@anarchy_server;" "-mod=@anarchy_client" -config=%ArmaConfigFolder%\server_config_AN.cfg -cfg=%ArmaConfigFolder%\basic_AN.cfg -profiles=%ArmaUserFolder% -name=Server_AN
+start "Server_AN" /high "arma3server_x64.exe" -filepatching -enableHT -malloc=system -autoInit -port=2302 -loadMissionToMemory -noPause -maxmem=3071 -noSound -bandwidthAlg=2 "-servermod=@anarchy_server;" "-mod=@anarchy_client;@vn;@task_force_radio;@CBA_A3;" "-config=%ArmaConfigFolder%\server_config_AN.cfg" "-cfg=%ArmaConfigFolder%\basic_AN.cfg" "-profiles=%ArmaUserFolder%" -name=Server_AN
 ::   /affinity F 
 
 :: timeout 5
