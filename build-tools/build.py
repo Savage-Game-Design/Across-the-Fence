6c6f17b11d5d45db1217bdb7f290ce266395f479
Spoffy <contact@spoffy.net>
2020-08-19 00:17:46 +0100

    Adds Steam Arma install autodetection

--- build-tools/build.py
+++ build-tools/build.py
@@ -6,6 +6,7 @@ from pathlib import Path, PurePath
 import shutil
 import subprocess
 import sys
+import vdf
 
 root_directory = Path(path.realpath(__file__)).parent.parent
 addon_prefix = "\\sgd\\anarchy"
@@ -14,6 +15,70 @@ log_directory = root_directory / 'build_logs' / (datetime.datetime.now().strftim
 prefix_directory = Path('P:\\') / addon_prefix
 missions_directory = root_directory / "missions"
 arma_mod_folder_name = "anarchy"
+
+def find_steam_library_paths():
+    steam_path = Path(os.environ["ProgramFiles(x86)"]) / "Steam"
+    if not steam_path.exists():
+        return (False, f"Unable to find Steam at {steam_path}")
+    library_file_path = steam_path / "steamapps" / "libraryfolders.vdf"
+    if not library_file_path.exists():
+        return (False, f"Unable to locate Steam libraries ({library_file_path} does not exist)")
+
+    steam_library_paths = [steam_path]
+    try:
+        with open(library_file_path, "r") as library_file:
+            lib_info = vdf.load(library_file)
+            for (key, value) in lib_info["LibraryFolders"].items():
+                #Check if the key is an integer - that means it's a library path
+                try:
+                    int(key)
+                    steam_library_paths.append(Path(value))
+                except ValueError:
+                    #Don't need to do anything. This just means the key/value pair isn't one we want.
+                    pass
+    except Exception as e:
+        return (False, f"An error occurred while loading Steam library info. {e}")
+
+    return (True, steam_library_paths)
+
+def is_arma_server_dir(path):
+    return (path / "arma3server.exe").exists() and not is_arma_client_dir(path)
+
+def is_arma_client_dir(path):
+    return (path / "arma3.exe").exists()
+
+def is_arma_tools_dir(path):
+    return (path / "Arma3Tools.exe").exists()
+
+def find_arma_install_dirs():
+    (success, result) = find_steam_library_paths()
+    if not success:
+        return (success, result)
+
+    arma_paths = {
+        "games": [],
+        "servers": [],
+        "tools": []
+    }
+
+    steam_library_paths = result
+    game_folder_root_paths = list(filter(lambda p: p.exists(), [path / "steamapps" / "common" for path in steam_library_paths]))
+    for root_path in game_folder_root_paths:
+        for game_dir in root_path.iterdir():
+            #Quick filter - saves the overhead of checking every directory for an exe - makes a noticable difference
+            if not game_dir.name.casefold().find("arma 3") == 0:
+                continue
+            if is_arma_client_dir(game_dir):
+                arma_paths["games"].append(game_dir)
+                continue
+            if is_arma_server_dir(game_dir):
+                arma_paths["servers"].append(game_dir)
+                continue
+            if is_arma_tools_dir(game_dir):
+                arma_paths["tools"].append(game_dir)
+                continue
+
+    return arma_paths
 
 def create_folder_if_not_exists(folder_path, verbose=True):
     if folder_path.exists():
@@ -36,6 +101,13 @@ def create_symlink(link_path,dest_path,is_directory=False):
         else:
             print("Error while creating symlinks. Possible fix: This script needs to be run as an administrator.")
             print("Raw error: ", e)
+
+def remove_if_symlink_to_here(link_path):
+    if link_path.is_symlink():
+        #if our link path begins with the path to our root directory, we consider it a link to inside this repo.
+        if root_directory.resolve().as_posix() in link_path.resolve().as_posix():
+            print(f"Unlinking {link_path}")
+            link_path.unlink()
 
 def build(mod_names, overwrite=False):
 
@@ -175,28 +247,66 @@ def arma_setup(raw_path,mod_names,link_missions=True,disable=False):
         print(f"ERROR: {arma_path} is not an Arma root directory")
         return False
 
-    arma_mod_folder_path = arma_path / arma_mod_folder_name
-    create_folder_if_not_exists(arma_mod_folder_path)
-    
-    print(f"==== Linking packed mods to Arma 3: {mod_names} ====")
-    for mod_name in mod_names:
-        try:
-            mod_path = output_directory / mod_name
-            link_path = arma_mod_folder_path / mod_name
-            create_symlink(link_path, mod_path, is_directory = True)
-        except FileNotFoundError as e:
-            print(f"WARNING: Mod {mod_name} cannot be linked - path does not exist {e.filename}")
+    arma_link_folder_path = arma_path / arma_mod_folder_name
 
-    if link_missions:
-        print(f"==== Linking missions to Arma 3 ====")
+    if not disable:
+        create_folder_if_not_exists(arma_link_folder_path)
+        
+        print(f"==== Linking packed mods to Arma 3: {mod_names} ====")
+        for mod_name in mod_names:
+            try:
+                mod_path = output_directory / mod_name
+                link_path = arma_link_folder_path / mod_name
+                create_symlink(link_path, mod_path, is_directory = True)
+            except FileNotFoundError as e:
+                print(f"WARNING: Mod {mod_name} cannot be linked - path does not exist {e.filename}")
+
+        if link_missions:
+            print(f"==== Linking missions to Arma 3 ====")
+            try:
+                for mission in missions_directory.iterdir():
+                    if not mission.is_dir():
+                        continue
+                    link_path = arma_path / "mpmissions" / mission.name
+                    create_symlink(link_path, mission, is_directory = True)
+            except FileNotFoundError as e:
+                print(f"WARNING: Cannot link missions, {e.filename} does not exist")
+    else:
+        print(f"==== Unlinking mods from Arma 3: {mod_names} ====")
+        for child_path in arma_link_folder_path.iterdir():
+            remove_if_symlink_to_here(child_path)
+        print(f"==== Unlinking missions from Arma 3 ====")
+        for child_path in (arma_path / "mpmissions").iterdir():
+            remove_if_symlink_to_here(child_path)
+
+def select(options):
+    print(f"Select an option below by typing its number. Press 'a' to choose all options, or 'q' for none:")
+    for index, option in enumerate(options):
+        print(f"    {index}: {option}")
+    
+    while True:
+        choice = input()
+        if choice.lower() == "a":
+            return options
+        if choice.lower() == "q":
+            return []
         try:
-            for mission in missions_directory.iterdir():
-                if not mission.is_dir():
-                    continue
-                link_path = arma_path / "mpmissions" / mission.name
-                create_symlink(link_path, mission, is_directory = True)
-        except FileNotFoundError as e:
-            print(f"WARNING: Cannot link missions, {e.filename} does not exist")
+            index = int(choice)
+            if index >= 0 and index < len(options):
+                return [options[index]]
+        except ValueError:
+            #Do nothing, it's just an invalid input
+            pass
+        print(f"'{choice}' is not a valid option")
+
+def choose_arma_paths(permit_dedicated=True):
+    arma_paths = find_arma_install_dirs()
+    usable_paths = arma_paths["games"]
+    if permit_dedicated:
+        usable_paths = arma_paths["games"] + arma_paths["servers"]
+    if len(usable_paths) == 0:
+        return []
+    return select(usable_paths)
     
 def subcommand_build(args):
     print("======================")
@@ -218,13 +328,29 @@ def subcommand_pdrive(args):
 
 def subcommand_filepatching(args):
     action = "Disabling" if args.disable else "Enabling"
-    print(f"{action} filepatching")
-    filepatching(args.path, args.disable)
+    print(f"==== {action} filepatching ====")
+    paths = args.paths
+    if args.autodetect:
+        print("Autodetecting Arma installations...")
+        paths = choose_arma_paths(permit_dedicated=True)
+
+    for path in paths:
+        print (f"Configuring filepatching for {path}")
+        filepatching(path, args.disable)
 
 def subcommand_arma_setup(args):
-    print(f"Setting up Arma for Anarchy development - linking mods and missions")
+    action = "unlinking" if args.disable else "linking"
+    print(f"==== Setting up Arma for Anarchy development - {action} mods and missions ====")
+    paths = args.paths
+    if args.autodetect:
+        print("Autodetecting Arma installations...")
+        paths = choose_arma_paths(permit_dedicated=True)
+
     mods = ["@anarchy_server", "@anarchy_client", "@ASC"]
-    arma_setup(args.path,mods,link_missions=True)
+
+    for path in paths:
+        print(f"\nSetting up Arma instance at path: {path}")
+        arma_setup(path,mods,link_missions=True,disable=args.disable)
 
 if __name__ == "__main__":
     raw_args = sys.argv[1:]
@@ -248,13 +374,19 @@ if __name__ == "__main__":
     pdrive_parser.set_defaults(func=subcommand_pdrive)
 
     filepatching_parser = subparsers.add_parser('filepatching', help="Set up filepatching")
-    filepatching_parser.add_argument("path", help="Path to Arma Client or Server root")
+    filepatching_parser.add_argument("paths", help="Path to Arma Client or Server root", nargs="*")
+    filepatching_parser.add_argument("-a", "--autodetect", help="Autodetect Arma installations done through Steam. Note: This ignores any paths passed in", action="store_const", const=True, default=False)
     filepatching_parser.add_argument('-d', '--disable', help="Disables previously set up filepatching", action="store_const", const=True, default=False)
     filepatching_parser.set_defaults(func=subcommand_filepatching)
 
     arma_setup_parser = subparsers.add_parser('arma-setup', help="Installs links to all necessary mods and missions in Arma")
-    arma_setup_parser.add_argument("path", help="Path to Arma Client or Server root")
+    arma_setup_parser.add_argument("paths", help="Path to Arma Client or Server root", nargs="*")
+    arma_setup_parser.add_argument("-a", "--autodetect", help="Autodetect Arma installations done through Steam. Note: This ignores any paths passed in", action="store_const", const=True, default=False)
+    arma_setup_parser.add_argument('-d', '--disable', help="Unlinks mods and missions from specified Arma install", action="store_const", const=True, default=False)
     arma_setup_parser.set_defaults(func=subcommand_arma_setup)
+
+    #start_game_parser = subparsers.add_parser('start-game', help="Starts a dedicated server and joins an Arma 3 client to it, with mods.")
+    #start_game_parser.set_defaults(func=subcommand_start_game)
 
     help_parser = subparsers.add_parser('help', help='Prints help')
     help_parser.set_defaults(func=lambda *args: parser.print_help())
