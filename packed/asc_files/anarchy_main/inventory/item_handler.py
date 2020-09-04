39858c0bdf98f48a9017a3090966f1db12e4be4f
Dscha <Eggi2105@googlemail.com>
2020-09-04 02:17:34 +0200

    Backend: item_create simplified to create the baseInfo for an Item, setting the ID and parent

--- packed/asc_files/anarchy_main/inventory/item_handler.py
+++ packed/asc_files/anarchy_main/inventory/item_handler.py
@@ -1,18 +1,8 @@
 import random
 from . import inv_handler, id_handler
 
-# standard Values for Items
-itemData = {
-        "item":    (0, [2, 2]),
-        "tool":    (1, [5, 5]),
-        "wpn_pri": (2, [4, 8]),
-        "wpn_sec": (3, [4, 4]),
-        "wpn_lau": (4, [4, 10]),
-    }
-
 ############
 
-# ToDo: add precise definitions, used in all Classes
 def check_size(size):
     # if given, check if X/Y are not < 1
     if size[0] < 1:
@@ -49,62 +39,23 @@ def inv_data_get(sData, invID):
         return [False, {}]
 
 
-def item_create(**kwargs):
+def item_create(parent: str = ""):
+    """
+
+    :param parent:  STR - Name of parent/base from itemData
+    :return:        ToDo
+    """
+
     item = {
-        "id":          id_handler.create_id(),   # Item ID - Will ALWAYS be generated!
-
-        "parentData": "",  # Base Item Data, the A3 UI can refer to (stored ItemData)
-        # ----------------------------------------------------------- #
-        "slot":        0,           # Baseclass in Arma
-        # Used in Arma to determine the slot, in which the Item can put it in!
-        # Description:
-        # ToDo: Add Description and slot-ID defines somewhere and link it here
-        # ----------------------------------------------------------- #
-        "class_name": "",           # Arma Cfg-name
-        "rarity":      0,           # Rarity
-        "hp_cur":      100,         # Current HP
-        "hp_max":      100,         # Max HP
-        "curInv":      "-1",        # ID of Inventory, that the Item is in
-        "addInvSpace": 0,           # does Item add Inventory space?
-        "actions": {
-            # [stringtable, functionName, fncType (called (0) or spawned (1)), additional params]
-            "a1": ["", "", 0, []],
-            },
-
-        # ----------------------------------------------------------- #
-        "size":        [4, 4],      # Size in Inventory
-        # Format: [ Size Rows, Size Columns ]
-        # Example: [2, 4] == 2 Rows/Y/height, 4 Cols/X/width
-        # Visualization:
-        # [1,1,1,1,0]
-        # [1,1,1,1,0]
-        # ----------------------------------------------------------- #
-
-        # ----------------------------------------------------------- #
-        "invPos":      [0, 0],      # TopLeft Position of the Item in the InventoryGrid
-        # Format: [ Row Pos, Col Pos]
-        # Example: [0,1] == First Row, 2nd "Column"/Index (since we start from 0)
-        # Visualization:
-        # [0,>>1<<,0,0,0]
-        # ----------------------------------------------------------- #
-
-        # ----------------------------------------------------------- #
-        "isFlipped":   0,           # 0/1 - Check if Item was flipped
-        # Argument is needed to check if the Item was flipped, while placing/moving it into the new Inventory
-        # (cleaning up an InvGrid).
-        # State: 0 == No changes during moving. Item will stay (example) at: [2 (row), 4 (col)]
-        # Visualization:
-        # [1,1,1,1,0]
-        # [1,1,1,1,0]
-        #
-        # State: 1 == The size will automatically switch Col and Row entries: [2 (row), 4 (col)] -->> [4 (row),2 (col)]
-        # Visualization:
-        # [1,1,0,0,0]
-        # [1,1,0,0,0]
-        # [1,1,0,0,0]
-        # [1,1,0,0,0]
-        # ----------------------------------------------------------- #
-        "attachments": {
+        "id":           id_handler.create_id(),        # Item ID - Will ALWAYS be generated!
+        "parent":       parent,           # Base Item Data, the A3 UI can refer to (stored ItemData)
+        "rarity":       0,           # Rarity
+        "hp_cur":       100,         # Current HP
+        "hp_max":       100,         # Max HP
+        "curInv":       "-1",        # ID of Inventory, that the Item is in
+        "invPos":       [0, 0],      # TopLeft Position of the Item in the InventoryGrid
+        "isFlipped":    0,           # 0/1 - Check if Item was flipped
+        "attachments":  {
             "scope": "",            # TODO: determine what makes more sense: ItemID or full itemData?
             "magazine": "",         # TODO: determine what makes more sense: ItemID or full itemData?
             "muzzle": "",           # TODO: determine what makes more sense: ItemID or full itemData?
@@ -113,8 +64,6 @@ def item_create(**kwargs):
             },
         }
 
-    item.update(kwargs)
-    # print(item)
     return item
 
 
