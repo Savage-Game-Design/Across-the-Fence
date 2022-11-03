00be0ca8d118718f9470d8e9f987e693acf84385
Cerebral <55196334+cerebrallolzy@users.noreply.github.com>
2022-11-03 18:55:24 -0500

    Add database base

---
+++ tour.sql
@@ -0,0 +1,20 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for players
+-- ----------------------------
+DROP TABLE IF EXISTS `players`;
+CREATE TABLE `players`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` bigint(20) NOT NULL,
+  `data` longtext CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL DEFAULT '[]',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of players
+-- ----------------------------
+INSERT INTO `players` VALUES (1, 76561199195828749, '[]');
+
+SET FOREIGN_KEY_CHECKS = 1;
