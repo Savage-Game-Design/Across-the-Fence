ed1891060b180c43781ed6c77070f3de6ce71f20
Filip Maciejewski <veteran29@users.noreply.github.com>
2022-12-04 22:48:56 +0100

    Add arma run python command and VSCode task #37hg3ca (#9)
    Co-authored-by: Spoffy <4805393+Spoffy@users.noreply.github.com>

--- build/config.example.py
+++ build/config.example.py
@@ -1,12 +1,32 @@
+import os
 from pathlib import Path
 from vgm.pbo import PBO
 
-username = "Spoffy"
+username = os.getlogin()
+arma_profile = r"dev_vgm"
+
+paradigm_path = Path(__file__).parent /  "paradigm"
+mission_output_path = rf"C:\Users\{username}\Documents\Arma 3 - Other Profiles\{arma_profile}\mpmissions\vgm.cam_lao_nam"
 
 output_paths = {
     "default": {
-        PBO.MISSION: Path(rf"C:\Users\{username}\Documents\Arma 3\mpmissions\vgm.cam_lao_nam"),
+        PBO.MISSION: Path(mission_output_path),
         PBO.CLIENT: None,
         PBO.SERVER: None,
     }
 }
+
+arma_exe_path = r"C:\Program Files (x86)\Steam\steamapps\common\Arma 3\arma3_x64.exe"
+arma_arg_mods = [
+    r"!Workshop\@VN (Testing Build)",
+    r"!Workshop\@Advanced Developer Tools",
+]
+arma_args = [
+    f"-name={arma_profile}",
+    "-noSplash",
+    "-noPause",
+    "-window",
+    "-showScriptErrors",
+    "-debug",
+    rf"{mission_output_path}\mission.sqm", # open mission in editor
+]
