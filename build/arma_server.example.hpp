9a3941d4c14739fad92b7a41c9fba042389083f5
Filip Maciejewski <veteran29@users.noreply.github.com>
2022-12-14 17:24:17 +0100

    Add Arma server launch command and connect param to client

---
+++ build/arma_server.example.hpp
@@ -0,0 +1,35 @@
+hostname="VGM Development";
+
+loopback = true;
+localClient[] = {"127.0.0.1"};
+
+admins[] = {
+    "76561197993041837", // veteran29
+    "76561198011383725"  // Spoffy
+};
+passwordAdmin = "admin";
+
+voteThreshold = 1;
+voteMissionPlayers = 2;
+allowedVoteCmds[] = {};
+
+
+armaUnitsTimeout = 1;
+allowedFilePatching = 2;
+battlEye = 0;
+persistent = 0;
+verifySignatures = 0;
+
+class Missions {
+    class vgm {
+        template = "vgm\MISSION_NAME";
+        difficulty = "regular";
+        class Params {};
+    };
+};
+
+class AdvancedOptions {
+    LogObjectNotFound = true;
+    SkipDescriptionParsing = false;
+    ignoreMissionLoadErrors = true;
+};
