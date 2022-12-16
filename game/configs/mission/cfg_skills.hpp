13014460643b376f6d9cf80ec3166ebde206a0fb
Filip Maciejewski <veteran29@users.noreply.github.com>
2022-12-16 16:15:22 +0100

    Update skill tree config
    It's more in line with design docs now

--- game/configs/mission/cfg_skills.hpp
+++ game/configs/mission/cfg_skills.hpp
@@ -1,7 +1,6 @@
 
 
-class vgm_skillTemplate
-{
+class vgm_skillTemplate {
     displayName = "SKILL NAME";
     description = "SKILL DESCRIPTION";
     icon = "";
@@ -14,30 +13,67 @@ class vgm_skillTemplate
     codeActivate = "systemChat 'skill activated'";
 };
 
-class vgm_skillTrees
-{
-    class riflemanTree
-    {
-        conditionUnlock = "true";
+class vgm_skillTrees {
+    class combatTree {
+        // rifleman skills
+        class skills {
+            class tier_1 {
+                class moreStamina: vgm_skillTemplate {
+                    codeApply = "_this setUnitTrait ['loadCoef', 2]";
+                };
 
-        class moreStamina: vgm_skillTemplate
-        {
-            codeApply = "_this setUnitTrait ['loadCoef', 2]";
+                class steadyAim: vgm_skillTemplate {
+                    codeApply = "_this setCustomAimCoef 0.5";
+                };
+            };
+
+            class tier_2 {
+                class skill_3: vgm_skillTemplate {};
+                class skill_4: vgm_skillTemplate {};
+            };
+
+            class tier_3 {
+                class skill_5: vgm_skillTemplate {};
+                class skill_6: vgm_skillTemplate {};
+            };
+
+            class tier_4 {
+                class skill_7: vgm_skillTemplate {};
+                class skill_8: vgm_skillTemplate {};
+            };
         };
 
-        class steadyAim: vgm_skillTemplate
-        {
-            codeApply = "_this setCustomAimCoef 0.5";
+        // specializations
+        class subtrees {
+            class medicTree {
+                class skills {
+                    class tier_1 {
+                        class medicTrait: vgm_skillTemplate {
+                            codeApply = "_this setUnitTrait ['Medic', true]";
+                        };
+                    };
+
+                    class tier_2 {
+                        class skill_3: vgm_skillTemplate {};
+                        class skill_4: vgm_skillTemplate {};
+                    };
+                };
+
+                // expertise
+                class subtrees {
+                    class surgeonTree {
+                        class skills {
+                            class tier_1 {
+                                class skill_1: vgm_skillTemplate {};
+                                class skill_2: vgm_skillTemplate {};
+                            };
+                        };
+                    };
+                };
+            };
         };
     };
 
-    class medicTree
-    {
-        conditionUnlock = "false";
-
-        class medicTrait: vgm_skillTemplate
-        {
-            codeApply = "_this setUnitTrait ['Medic', true]";
-        };
-    };
+    class utility {};
+    class social {};
 };
