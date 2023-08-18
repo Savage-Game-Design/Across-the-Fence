732c4fbb68138443ed6f2e4b36f94ff07505e21b
Filip Maciejewski <veteran29@users.noreply.github.com>
2023-08-18 15:56:05 +0200

    Add VGM_RscHealthTextures

--- game/configs/mission/ui/ui_main.hpp
+++ game/configs/mission/ui/ui_main.hpp
@@ -8,9 +8,15 @@
 #include "VGM_DisplayAbilities.hpp"
 #include "VGM_DisplayMissions.hpp"
 #include "VGM_DisplayMissionsTargets.hpp"
+
+import RscHealthTextures from RscTitles;
 class RscTitles
 {
     #include "VGM_RscAbilityCooldown.hpp"
     #include "VGM_RscProgressBar.hpp"
+    class VGM_RscHealthTextures: RscHealthTextures
+    {
+        onLoad = "uiNamespace setVariable ['vgm_RscHealthTextures', _this select 0]";
+    };
 };
 #include "VGM_DisplayTest.hpp"
