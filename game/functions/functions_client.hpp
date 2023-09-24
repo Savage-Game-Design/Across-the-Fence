bc628f5e1f8213964a9a0821c427d9dbb4691bf4
Filip Maciejewski <veteran29@users.noreply.github.com>
2023-09-24 21:52:22 +0200

    Refactor skill functions into client/server folders

--- game/functions/functions_client.hpp
+++ game/functions/functions_client.hpp
@@ -325,7 +325,7 @@ class vgm_c
 
     class skill_passives
     {
-        VGM_CLIENT_PATH(\systems\skill\passives);
+        VGM_CLIENT_PATH(\systems\skill\client\passives);
 
         class skill_passives_preInit
         {
@@ -334,14 +334,14 @@ class vgm_c
     };
     class skill_passives_infantryman
     {
-        VGM_CLIENT_PATH(\systems\skill\passives\infantryman);
+        VGM_CLIENT_PATH(\systems\skill\client\passives\infantryman);
 
         class skill_passives_infantryman_bornLeader {};
         class skill_passives_infantryman_overprepared {};
     };
     class skill_passives_recon
     {
-        VGM_CLIENT_PATH(\systems\skill\passives\recon);
+        VGM_CLIENT_PATH(\systems\skill\client\passives\recon);
 
         class skill_passives_recon_betterAim {};
         class skill_passives_recon_keenEye {};
@@ -349,11 +349,11 @@ class vgm_c
 
     class skill_actives
     {
-        VGM_CLIENT_PATH(\systems\skill\actives);
+        VGM_CLIENT_PATH(\systems\skill\client\actives);
     };
     class skill_actives_infantryman
     {
-        VGM_CLIENT_PATH(\systems\skill\actives\infantryman);
+        VGM_CLIENT_PATH(\systems\skill\client\actives\infantryman);
 
         class skill_actives_infantryman_luckySon {};
         class skill_actives_infantryman_steadyAim {};
---
+++ game/functions/systems/skill/server/fn_skill_preInit.sqf
@@ -0,0 +1,12 @@
+/*
+    File: fn_skill_passives_preInit.sqf
+    Author: Savage Game Design
+    Date: 2023-09-24
+    Last Update: 2023-09-24
+    Public: No
+
+    Description:
+        Server preInit function for skill implementations.
+ */
+
+if (!isServer) exitWith {};
