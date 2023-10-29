0cb5f296820afadd0ec69399d50b131a5be9aaab
Filip Maciejewski <veteran29@users.noreply.github.com>
2023-10-29 16:07:44 +0100

    Shorten default XP breakdown text

--- game/configs/mission/ui/VGM_DisplayEndOfMission.hpp
+++ game/configs/mission/ui/VGM_DisplayEndOfMission.hpp
@@ -103,7 +103,7 @@ class VGM_DisplayEndOfMission
         class XpBreakdown: VGM_ctrlStructuredText
         {
             idc = VGM_IDC_DISPLAYENDOFMISSION_XPBREAKDOWN;
-            text = "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.";
+            text = "XP Breakdown text";
             x = DX + 1 * VGM_GRID_W;
             y = VGM_Y_Y(DY, SPACING);
             w = (DW - 2) * VGM_GRID_W;
