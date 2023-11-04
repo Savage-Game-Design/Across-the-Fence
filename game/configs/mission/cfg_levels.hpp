4b82e0662c188b2c60a93d79131ba9e739460fc2
Spoffy <contact@spoffy.net>
2023-11-04 14:46:02 +0000

    Rebalances the amount of skill points available in Alpha

--- game/configs/mission/cfg_levels.hpp
+++ game/configs/mission/cfg_levels.hpp
@@ -5,135 +5,136 @@ class vgm_levels {
     };
     class level_1 {
         experience = 500; // amount of XP needed to advance to next level
-        skillPoints = 5; // amount of SP gained when reaching the level
+        skillPoints = 1; // amount of SP gained when reaching the level
     };
     class level_2 {
         experience = 575;
-        skillPoints = 5;
+        skillPoints = 1;
     };
     class level_3 {
         experience = 646;
-        skillPoints = 5;
+        skillPoints = 1;
     };
     class level_4 {
         experience = 713;
-        skillPoints = 5;
+        skillPoints = 1;
     };
     class level_5 {
         experience = 778;
-        skillPoints = 5;
+        skillPoints = 1;
     };
     class level_6 {
         experience = 839;
-        skillPoints = 5;
+        skillPoints = 1;
     };
     class level_7 {
         experience = 897;
-        skillPoints = 5;
+        skillPoints = 1;
     };
     class level_8 {
         experience = 952;
-        skillPoints = 5;
+        skillPoints = 1;
     };
     class level_9 {
         experience = 1004;
-        skillPoints = 5;
+        skillPoints = 1;
     };
     class level_10 {
         experience = 1054;
-        skillPoints = 5;
+        skillPoints = 1;
     };
     class level_11 {
         experience = 1101;
-        skillPoints = 5;
+        skillPoints = 1;
     };
     class level_12 {
         experience = 1146;
-        skillPoints = 5;
+        skillPoints = 1;
     };
     class level_13 {
         experience = 1189;
-        skillPoints = 4;
+        skillPoints = 1;
     };
     class level_14 {
         experience = 1229;
-        skillPoints = 4;
+        skillPoints = 1;
     };
     class level_15 {
         experience = 1268;
-        skillPoints = 4;
+        skillPoints = 1;
     };
+    /*
     class level_16 {
         experience = 1305;
-        skillPoints = 4;
+        skillPoints = 1;
     };
     class level_17 {
         experience = 1339;
-        skillPoints = 4;
+        skillPoints = 1;
     };
     class level_18 {
         experience = 1372;
-        skillPoints = 4;
+        skillPoints = 1;
     };
     class level_19 {
         experience = 1404;
-        skillPoints = 4;
+        skillPoints = 1;
     };
     class level_20 {
         experience = 1433;
-        skillPoints = 4;
+        skillPoints = 1;
     };
     class level_21 {
         experience = 1462;
-        skillPoints = 4;
+        skillPoints = 1;
     };
     class level_22 {
         experience = 1489;
-        skillPoints = 4;
+        skillPoints = 1;
     };
     class level_23 {
         experience = 1514;
-        skillPoints = 4;
+        skillPoints = 1;
     };
     class level_24 {
         experience = 1538;
-        skillPoints = 3;
+        skillPoints = 1;
     };
     class level_25 {
         experience = 1562;
-        skillPoints = 3;
+        skillPoints = 1;
     };
     class level_26 {
         experience = 1583;
-        skillPoints = 3;
+        skillPoints = 1;
     };
     class level_27 {
         experience = 1604;
-        skillPoints = 3;
+        skillPoints = 1;
     };
     class level_28 {
         experience = 1624;
-        skillPoints = 3;
+        skillPoints = 1;
     };
     class level_29 {
         experience = 1643;
-        skillPoints = 2;
+        skillPoints = 1;
     };
     class level_30 {
         experience = 1661;
-        skillPoints = 2;
+        skillPoints = 1;
     };
     class level_31 {
         experience = 1678;
-        skillPoints = 2;
+        skillPoints = 1;
     };
     class level_32 {
         experience = 1694;
-        skillPoints = 2;
+        skillPoints = 1;
     };
     class level_33 {
         experience = 1709;
-        skillPoints = 2;
+        skillPoints = 1;
     };
     class level_34 {
         experience = 1723;
@@ -163,4 +164,5 @@ class vgm_levels {
         experience = 1797;
         skillPoints = 1;
     };
+    */
 };
