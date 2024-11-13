227c6b2a19f9c65b3091d85decbbdab8ebfc0481
Filip Maciejewski <veteran29@users.noreply.github.com>
2024-11-13 20:50:58 +0100

    Add field manual config

---
+++ game/configs/mission/cfg_field_manual.hpp
@@ -0,0 +1,10 @@
+
+class CfgHints {
+    class vgm {
+        displayName = "$STR_VGM_MISSION_NAME";
+
+        class getting_started {
+            displayName = "$STR_VGM_FIELD_MANUAL_GETTING_STARTED";
+        };
+    };
+};
