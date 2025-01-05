f2962b1d447c1c975fe226c19f578bf7ba916e52
Filip Maciejewski <veteran29@users.noreply.github.com>
2025-01-05 23:42:30 +0100

    Update indicator layout

--- game/configs/mission/ui/VGM_RscLevelIndicator.hpp
+++ game/configs/mission/ui/VGM_RscLevelIndicator.hpp
@@ -14,73 +14,66 @@ class VGM_RscLevelIndicator
             x = GUI_GRID_TOPCENTER_X + 10 * GUI_GRID_W;
             y = GUI_GRID_TOPCENTER_Y + 0.5 * GUI_GRID_H;
             w = 20 * GUI_GRID_W;
-            h = 1 * GUI_GRID_H;
+            h = 1.5 * GUI_GRID_H;
 
             class Controls
             {
-                class Test: RscText
+                class Background: RscText
                 {
                     idc = -1;
                     text = "";
                     x = 0;
                     y = 0;
                     w = 20 * GUI_GRID_W;
-                    h = 1 * GUI_GRID_H;
+                    h = 1.5 * GUI_GRID_H;
 
-                    colorBackground[] = {1,0,0,1};
-                };
-
-                class LevelCurrent: VGM_ctrlStructuredText
-                {
-                    idc = -1;
-                    text = "00";
-                    x = 0;
-                    y = 0;
-                    w = 2 * GUI_GRID_W;
-                    h = 1 * GUI_GRID_H;
-
-                    class Attributes: Attributes
-                    {
-                        align = "right";
-                        shadow = 1;
-                    };
+                    colorBackground[] = {0,0,0,0.4};
                 };
 
                 class XpProgress: VGM_ctrlProgress
                 {
                     idc = -1;
-                    x = 2 * GUI_GRID_W;
+                    x = 0 * GUI_GRID_W;
                     y = 0;
-                    w = 16 * GUI_GRID_W;
-                    h = 1 * GUI_GRID_H;
-                    colorFrame[] = {1,1,1,1};
-                };
-                class XpText: VGM_ctrlStructuredText
-                {
-                    idc = -1;
-                    text = "- / -";
-                    x = 2 * GUI_GRID_W;
-                    y = 0;
-                    w = 16 * GUI_GRID_W;
-                    h = 1 * GUI_GRID_H;
-                    class Attributes
-                    {
-                        font = VGM_FONT;
-                        color = "#ffffff";
-                        colorLink = "#D09B43";
-                        align = "center";
-                        shadow = 1;
-                    };
+                    w = 20 * GUI_GRID_W;
+                    h = 0.7 * GUI_GRID_H;
+
+                    colorFrame[] = {0,0,0,1};
                 };
 
-                class LevelNext: LevelCurrent
+                class LevelCurrent: VGM_ctrlStructuredText
                 {
                     idc = -1;
-                    x = 2 * GUI_GRID_W + 16 * GUI_GRID_W;
+                    text = "Level 00";
+                    x = 0;
+                    y = 0.65 * GUI_GRID_H;
+                    w = 6 * GUI_GRID_W;
+                    h = 0.8 * GUI_GRID_H;
+
+                    size = 0.8 * GUI_GRID_H;
 
                     class Attributes: Attributes
                     {
                         align = "left";
+                        shadow = 1;
+                    };
+                };
+
+                class XpText: VGM_ctrlStructuredText
+                {
+                    idc = -1;
+                    text = "- / - XP";
+                    x = 6 * GUI_GRID_W;
+                    y = 0.65 * GUI_GRID_H;
+                    w = 14 * GUI_GRID_W;
+                    h = 0.8 * GUI_GRID_H;
+
+                    size = 0.8 * GUI_GRID_H;
+
+                    class Attributes: Attributes
+                    {
+                        align = "right";
+                        shadow = 1;
                     };
                 };
 
