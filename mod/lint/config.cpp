19fe013a26de8a5a04606429697bb5a5f8391ee0
Filip Maciejewski <veteran29@users.noreply.github.com>
2025-10-01 23:39:43 +0200

    Add hemtt configs for linting

---
+++ mod/lint/config.cpp
@@ -0,0 +1,18 @@
+// This is automatically added by the build system
+#include "common_includes.hpp"
+
+class CfgPatches
+{
+    class ADDON
+    {
+        AUTHORSAVAGE;
+        name = "server";
+        url = "https://www.sogpf.com";
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 2.18;
+        requiredAddons[] = {};
+    };
+};
+
+#include "description.ext"
