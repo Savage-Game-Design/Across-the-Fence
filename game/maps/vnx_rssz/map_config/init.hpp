c5d02374e6fb1961cf446f3083cc1647ff67edbc
Spoffy <4805393+Spoffy@users.noreply.github.com>
2025-12-14 00:49:24 +0000

    Adds Rung Sat Special Zone

---
+++ game/maps/vnx_rssz/map_config/init.hpp
@@ -0,0 +1,9 @@
+class MapConfig {
+    // Sets the maximum visibility a player can have when prone on a surface with this clutter type.
+    // Types are found in CfgSurfaceCharacters.
+    class Stealth_Prone_GroundClutterVisibilityMax {
+        vn_GrassTallClutter = 0.7;
+        vn_RainForestClutter = 0.8;
+        vn_RiceClutter = 0.6;
+    };
+};
